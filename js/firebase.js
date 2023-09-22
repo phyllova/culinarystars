63595841f4c9b193baf179bb4eb281c68c686fff
Phyllova <127481743+phyllova@users.noreply.github.com>
2023-09-22 01:48:59 -0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,15 @@
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDhn80byD69jCo4oexdKdkCHUgGw9nO6CM",
-  authDomain: "dubai4db.firebaseapp.com",
-  databaseURL: "https://dubai4db-default-rtdb.firebaseio.com",
-  projectId: "dubai4db",
-  storageBucket: "dubai4db.appspot.com",
-  messagingSenderId: "776520121975",
-  appId: "1:776520121975:web:248815949672b2c4371072"
+  apiKey: "AIzaSyD3C36ehM2sfFVJRB42bBp93X0RJBIrZA0",
+  authDomain: "asrabtarz.firebaseapp.com",
+  databaseURL: "https://asrabtarz-default-rtdb.firebaseio.com",
+  projectId: "asrabtarz",
+  storageBucket: "asrabtarz.appspot.com",
+  messagingSenderId: "311983543030",
+  appId: "1:311983543030:web:480a502147bc8be0f4d2d7",
+  measurementId: "G-33BGZJFXYS"
 };
+
 firebase.initializeApp(firebaseConfig);
 const appCheck = firebase.appCheck();
 console.log(appCheck);
